8145484740104fbdbe48082fb8caad66debfd42e
karata-ai <nikola.karayanev59@gmail.com>
2025-06-08 13:17:15 +0100

    Update pokemon_classifier.py

--- pokemon_classifier.py
+++ pokemon_classifier.py
@@ -1,13 +1,21 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+import os
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
-# importing the data
-df = pd.read_csv('dataset_.csv')
+## importing the data
+# get the directory where the current script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# build full path to the CSV file in the same folder as script
+csv_path = os.path.join(script_dir, 'dataset_.csv')
+
+# load the CSV using the full path
+df = pd.read_csv(csv_path)
 
 # data clearing and editing
 df = df.dropna()
@@ -15,10 +23,13 @@ counts = df['Type 1'].value_counts()
 valid_types = counts[counts >= 5].index
 df = df[df['Type 1'].isin(valid_types)]
 
+# encoding the string type data
+le = LabelEncoder()
+
 # choosing the features, X and y
-features = ['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']
+features = ['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Total', 'Legendary']
 X = df[features]
-y = df['Type 1']
+y = le.fit_transform(df['Type 1'])
 
 # scaling
 scaler = StandardScaler()
@@ -30,8 +41,8 @@ X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, 
 # list of models
 models = {
     'KNN (k=5)': KNeighborsClassifier(n_neighbors=5),
-    'SVM (linear)': SVC(kernel='linear'),
-    'Random Forest': RandomForestClassifier(n_estimators=100, random_state=42)
+    'SVM (linear)': SVC(kernel='linear', class_weight='balanced'),
+    'Random Forest': RandomForestClassifier(n_estimators=100, random_state=42, class_weight='balanced')
 }
 
 # model predictions and score
